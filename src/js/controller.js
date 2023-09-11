47ff90358b78147edccf531d2ad3570c2ca7cd65
alecccg03 <alecg03@comcast.net>
2023-09-11 09:39:29 -0700

    Back to normal app

--- src/js/controller.js
+++ src/js/controller.js
@@ -106,10 +106,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Git practice');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -118,6 +114,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
